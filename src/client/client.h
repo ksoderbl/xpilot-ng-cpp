f9893d8ca73c3b792c57a147dcf762f95cd63403
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2026-07-12 00:09:30 +0300

    client: Move some Handle functions from client.cpp to clientmap.cpp.

--- src/client/client.h
+++ src/client/client.h
@@ -454,50 +454,50 @@ int Handle_score_object(double score, int x, int y, char *msg);
 int Handle_team_score(int team, double score);
 int Handle_timing(int id, int check, int round, long loops);
 int Handle_seek(int programmer_id, int robot_id, int sought_id);
-int Handle_start(long server_loops);
-int Handle_end(long server_loops);
-int Handle_self(int x, int y, int vx, int vy, int newHeading,
-                double newPower, double newTurnspeed, double newTurnresistance,
-                int newLockId, int newLockDist, int newLockBearing,
-                int newNextCheckPoint, int newAutopilotLight,
-                uint8_t *newNumItems, int newCurrentTank,
-                double newFuelSum, double newFuelMax, int newPacketSize,
-                int status);
-int Handle_self_items(uint8_t *newNumItems);
-int Handle_modifiers(char *m);
-int Handle_damaged(int dam);
-int Handle_destruct(int count);
-int Handle_shutdown(int count, int delay);
-int Handle_thrusttime(int count, int max);
-int Handle_shieldtime(int count, int max);
-int Handle_phasingtime(int count, int max);
-int Handle_rounddelay(int count, int max);
-int Handle_refuel(int x0, int y0, int x1, int y1);
-int Handle_connector(int x0, int y0, int x1, int y1, int tractor);
-int Handle_laser(int color, int x, int y, int len, int dir);
-int Handle_missile(int x, int y, int dir, int len);
-int Handle_ball(int x, int y, int id, int style);
-int Handle_ship(int x, int y, int id, int dir, int shield, int cloak,
-                int eshield, int phased, int deflector);
-int Handle_mine(int x, int y, int teammine, int id);
-int Handle_item(int x, int y, int type);
-int Handle_fastshot(int type, uint8_t *p, int n);
-int Handle_teamshot(int type, uint8_t *p, int n);
-int Handle_debris(int type, uint8_t *p, int n);
-int Handle_wreckage(int x, int y, int wrecktype, int size, int rotation);
-int Handle_asteroid(int x, int y, int type, int size, int rotation);
-int Handle_wormhole(int x, int y);
-int Handle_polystyle(int polyind, int newstyle);
-int Handle_ecm(int x, int y, int size);
-int Handle_trans(int x1, int y1, int x2, int y2);
-int Handle_paused(int x, int y, int count);
-int Handle_appearing(int x, int y, int id, int count);
-int Handle_radar(int x, int y, int size);
-int Handle_fastradar(int x, int y, int size);
-int Handle_vcannon(int x, int y, int type);
-int Handle_vfuel(int x, int y, double fuel);
-int Handle_vbase(int x, int y, int xi, int yi, int type);
-int Handle_vdecor(int x, int y, int xi, int yi, int type);
+// int Handle_start(long server_loops);
+// int Handle_end(long server_loops);
+// int Handle_self(int x, int y, int vx, int vy, int newHeading,
+//                 double newPower, double newTurnspeed, double newTurnresistance,
+//                 int newLockId, int newLockDist, int newLockBearing,
+//                 int newNextCheckPoint, int newAutopilotLight,
+//                 uint8_t *newNumItems, int newCurrentTank,
+//                 double newFuelSum, double newFuelMax, int newPacketSize,
+//                 int status);
+// int Handle_self_items(uint8_t *newNumItems);
+// int Handle_modifiers(char *m);
+// int Handle_damaged(int dam);
+// int Handle_destruct(int count);
+// int Handle_shutdown(int count, int delay);
+// int Handle_thrusttime(int count, int max);
+// int Handle_shieldtime(int count, int max);
+// int Handle_phasingtime(int count, int max);
+// int Handle_rounddelay(int count, int max);
+// int Handle_refuel(int x0, int y0, int x1, int y1);
+// int Handle_connector(int x0, int y0, int x1, int y1, int tractor);
+// int Handle_laser(int color, int x, int y, int len, int dir);
+// int Handle_missile(int x, int y, int dir, int len);
+// int Handle_ball(int x, int y, int id, int style);
+// int Handle_ship(int x, int y, int id, int dir, int shield, int cloak,
+//                 int eshield, int phased, int deflector);
+// int Handle_mine(int x, int y, int teammine, int id);
+// int Handle_item(int x, int y, int type);
+// int Handle_fastshot(int type, uint8_t *p, int n);
+// int Handle_teamshot(int type, uint8_t *p, int n);
+// int Handle_debris(int type, uint8_t *p, int n);
+// int Handle_wreckage(int x, int y, int wrecktype, int size, int rotation);
+// int Handle_asteroid(int x, int y, int type, int size, int rotation);
+// int Handle_wormhole(int x, int y);
+// int Handle_polystyle(int polyind, int newstyle);
+// int Handle_ecm(int x, int y, int size);
+// int Handle_trans(int x1, int y1, int x2, int y2);
+// int Handle_paused(int x, int y, int count);
+// int Handle_appearing(int x, int y, int id, int count);
+// int Handle_radar(int x, int y, int size);
+// int Handle_fastradar(int x, int y, int size);
+// int Handle_vcannon(int x, int y, int type);
+// int Handle_vfuel(int x, int y, double fuel);
+// int Handle_vbase(int x, int y, int xi, int yi, int type);
+// int Handle_vdecor(int x, int y, int xi, int yi, int type);
 int Handle_message(char *msg);
 int Handle_eyes(int id);
 int Handle_time_left(long sec);
--- src/client/clientmap.h
+++ src/client/clientmap.h
@@ -344,3 +344,48 @@ extern int num_polygon_styles, max_polygon_styles;
 // extern int num_asteroids, max_asteroids;
 // extern wormhole_t *wormhole_ptr;
 // extern int num_wormholes, max_wormholes;
+
+int Handle_start(long server_loops);
+int Handle_end(long server_loops);
+int Handle_self(int x, int y, int vx, int vy, int newHeading,
+                double newPower, double newTurnspeed, double newTurnresistance,
+                int newLockId, int newLockDist, int newLockBearing,
+                int newNextCheckPoint, int newAutopilotLight,
+                uint8_t *newNumItems, int newCurrentTank,
+                double newFuelSum, double newFuelMax, int newPacketSize,
+                int status);
+int Handle_self_items(uint8_t *newNumItems);
+int Handle_modifiers(char *m);
+int Handle_damaged(int dam);
+int Handle_destruct(int count);
+int Handle_shutdown(int count, int delay);
+int Handle_thrusttime(int count, int max);
+int Handle_shieldtime(int count, int max);
+int Handle_phasingtime(int count, int max);
+int Handle_rounddelay(int count, int max);
+int Handle_refuel(int x0, int y0, int x1, int y1);
+int Handle_connector(int x0, int y0, int x1, int y1, int tractor);
+int Handle_laser(int color, int x, int y, int len, int dir);
+int Handle_missile(int x, int y, int dir, int len);
+int Handle_ball(int x, int y, int id, int style);
+int Handle_ship(int x, int y, int id, int dir, int shield, int cloak,
+                int eshield, int phased, int deflector);
+int Handle_mine(int x, int y, int teammine, int id);
+int Handle_item(int x, int y, int type);
+int Handle_fastshot(int type, uint8_t *p, int n);
+int Handle_teamshot(int type, uint8_t *p, int n);
+int Handle_debris(int type, uint8_t *p, int n);
+int Handle_wreckage(int x, int y, int wrecktype, int size, int rotation);
+int Handle_asteroid(int x, int y, int type, int size, int rotation);
+int Handle_wormhole(int x, int y);
+int Handle_polystyle(int polyind, int newstyle);
+int Handle_ecm(int x, int y, int size);
+int Handle_trans(int x1, int y1, int x2, int y2);
+int Handle_paused(int x, int y, int count);
+int Handle_appearing(int x, int y, int id, int count);
+int Handle_radar(int x, int y, int size);
+int Handle_fastradar(int x, int y, int size);
+int Handle_vcannon(int x, int y, int type);
+int Handle_vfuel(int x, int y, double fuel);
+int Handle_vbase(int x, int y, int xi, int yi, int type);
+int Handle_vdecor(int x, int y, int xi, int yi, int type);
